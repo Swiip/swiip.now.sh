20a95c8f49a0a5195e05c08aadde55533bb3d734
Swiip <matthieu.lux@gmail.com>
2019-05-05 09:43:45 +0200

    add animation on home page

--- packages/client/public/components/home.js
+++ packages/client/public/components/home.js
@@ -1,5 +1,5 @@
 import html from "/html.js";
-import style from "/style.js";
+import style, { keyframes } from "/style.js";
 
 const Container = style("article")({
   display: "flex",
@@ -8,10 +8,36 @@ const Container = style("article")({
   alignItems: "center"
 });
 
+const bounceInRight = keyframes({
+  from: {
+    opacity: "0",
+    transform: "translate3d(300px, 0, 0)"
+  },
+
+  "75%": {
+    opacity: "1",
+    transform: "translate3d(-10px, 0, 0)"
+  },
+
+  to: {
+    opacity: "1",
+    transform: "translate3d(0, 0, 0)"
+  }
+});
+
 const Line = style("h2")({
   fontSize: "35px",
   fontWeight: "normal",
-  margin: "10px 0"
+  margin: "10px 0",
+  opacity: "0",
+  animation: `${bounceInRight} .3s forwards`,
+  animationDelay: ".3s",
+  ":nth-child(2)": {
+    animationDelay: ".4s"
+  },
+  ":nth-child(3)": {
+    animationDelay: ".5s"
+  }
 });
 
 const Home = () => html`
--- packages/client/public/style.js
+++ packages/client/public/style.js
@@ -1,6 +1,8 @@
 import { h } from "/imports/preact.js";
 import picostyle from "/imports/picostyle.js";
 
+export { keyframes } from "/imports/picostyle.js";
+
 const style = picostyle(h);
 
 export default style;
